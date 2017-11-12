f06f6c1ff5774cc4e5973ef630f0710262979466
Jolita Arnaudova <jolita.arnaudova@gmail.com>
2017-11-12 19:27:18 +0100

    Setup middleware to prevent unauthorized users from editing and deleting campgrounds, they do not own

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -51,18 +51,14 @@ router.get("/campgrounds/:id", function(req, res) {
 });
 
 // Render template for editing a campground
-router.get("/campgrounds/:id/edit", function (req, res) {
-    Campground.findById(req.params.id, function (err, foundCampground) {
-        if (err) {
-            res.redirect("/campgrounds")
-        } else {
+router.get("/campgrounds/:id/edit", checkCampgroundOwnerShip, function (req, res) {
+        Campground.findById(req.params.id, function (err, foundCampground) {
             res.render("campgrounds/edit", {campground: foundCampground});
-        }
-    });
+        });
 });
 
 // Update the previously edited campground
-router.put("/campgrounds/:id", function (req, res) {
+router.put("/campgrounds/:id", checkCampgroundOwnerShip, function (req, res) {
    // find and update the correct cmpground
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function (err, upatedCampground) {
         if (err) {
@@ -74,7 +70,7 @@ router.put("/campgrounds/:id", function (req, res) {
 });
 
 // Delete campground
-router.delete("/campgrounds/:id", function (req, res) {
+router.delete("/campgrounds/:id", checkCampgroundOwnerShip, function (req, res) {
    Campground.findByIdAndRemove(req.params.id, function (err) {
        if (err) {
            res.redirect("/campgrounds");
@@ -90,5 +86,24 @@ function isLoggedIn(req, res, next) {
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCampgroundOwnerShip(req, res, next) {
+    if (req.isAuthenticated()) {
+        Campground.findById(req.params.id, function (err, foundCampground) {
+            if (err) {
+                res.redirect("back")
+            } else {
+                // does user own campground?
+                if (foundCampground.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
 }
 module.exports = router;
